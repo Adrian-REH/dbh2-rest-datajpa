4fabbb386d74e3d3682d3f50e95a8cfbddec5a13
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2023-04-10 21:18:41 +0200

    Add files via upload

---
+++ dbh2-rest-datajpa/src/main/java/com/example/dbh2restdatajpa/Dbh2RestDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbh2restdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbh2RestDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dbh2RestDatajpaApplication.class, args);
+	}
+
+}
---
+++ dbh2-rest-datajpa/src/test/java/com/example/dbh2restdatajpa/Dbh2RestDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbh2restdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dbh2RestDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
