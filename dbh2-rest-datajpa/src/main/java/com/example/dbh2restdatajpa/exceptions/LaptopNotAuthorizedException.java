802e13ba71d745d5b6dce82162cc665b92306a41
Adrian-REH <adrianherrera.r.e@gmail.com>
2023-04-17 21:33:46 +0200

    fin

--- dbh2-rest-datajpa/src/main/java/com/example/dbh2restdatajpa/exceptions/LaptopNotAuthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.dbh2restdatajpa.exceptions;
-
-public class LaptopNotAuthorizedException extends Exception {
-    public LaptopNotAuthorizedException(String message) {
-        super(message);
-    }
-}
--- dbh2-rest-datajpa/src/main/java/com/example/dbh2restdatajpa/repositories/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dbh2restdatajpa.repositories;
-
-import com.example.dbh2restdatajpa.entities.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaptopRepository extends JpaRepository<Laptop,Long> {
-}
